a20f352cc8fa3c29318ba2968f09d916cefee4cc
Manuel Berger <37898579+ggmanugg@users.noreply.github.com>
2018-07-11 15:59:34 +0200

    Update duckdns.sh

--- duckdns.sh
+++ duckdns.sh
@@ -69,8 +69,9 @@ sudo ./dehydrated --register  --accept-terms
 sudo ./dehydrated -c
 
 #Add certificate to home assistant
-#cert="~/dehydrated/certs/"$domain"/fullchain.pem/"
-#sed -i '/base_url/s/.*/  base_url: $domain:8123\n  ssl_certificate: $cert/' ~/.homeassistant/configuration.yaml
+cert=$(echo $(sudo find /home/admin/dehydrated/certs/ -name "fullchain.pem"))
+key=$(echo $(sudo find /home/admin/dehydrated/certs/ -name "privkey.pem*"))
+sed -i '/base_url/s/.*/  base_url: $domain:8123\n  ssl_certificate: $cert\n  ssl_key: $key/' ~/.homeassistant/configuration.yaml
 
 #Restart home assistant
 sudo systemctl restart homeassistant@$usern.service
