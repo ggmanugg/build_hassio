aa1f5fb2a9c13abeb43dc020570bd7dab34cfc2f
Manuel Berger <37898579+ggmanugg@users.noreply.github.com>
2018-07-13 08:49:33 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -48,6 +48,7 @@ sudo apt-get upgrade -y
 sudo apt-get install python3-pip python3-venv -y
 
 #Setup virtual environment
+cd ..
 sudo python3 -m venv homeassistant
 
 source /home/"$usern"/homeassistant/bin/activate && python3 -m pip install wheel
@@ -125,4 +126,5 @@ cat << "EOF"
  |  _|  | || |\  || | ___) |  _  |
  |_|   |___|_| \_|___|____/|_| |_|    
 EOF
+echo \n
 echo Go to https://ipaddress:8123 and enjoy your Home Assistant
