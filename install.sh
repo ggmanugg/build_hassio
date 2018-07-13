8dd6e307af6d79ae0f0c08951dcd08032ba5d429
Manuel Berger <37898579+ggmanugg@users.noreply.github.com>
2018-07-13 09:10:06 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -48,8 +48,7 @@ sudo apt-get upgrade -y
 sudo apt-get install python3-pip python3-venv -y
 
 #Setup virtual environment
-cd ..
-sudo python3 -m venv homeassistant
+sudo python3 -m venv homeassistant /home/"$usern"/
 
 source /home/"$usern"/homeassistant/bin/activate && python3 -m pip install wheel
 
